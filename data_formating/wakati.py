ea425b0042573c2a3a012de1061ea303feb67ca1
ritsukishuto <rtk410s@gmail.com>
2022-07-11 16:16:25 +0900

    微修正

--- data_formating/wakati.py
+++ data_formating/wakati.py
@@ -8,13 +8,12 @@ import MeCab as mecab
 from gensim.corpora import Dictionary
 from gensim import matutils as mtu
 
+df = pd.read_csv('../train_data/trans/text-only.csv', encoding='utf-8', header=0)       # csvを読み込む
+
+m = mecab.Tagger("-d /opt/mecab/lib/mecab/dic/neologd -Owakati")       # NEologd
+#m = mecab.Tagger('-Owakati')        # ipadic
+
 wakati_list = []
-#m = mecab.Tagger("-d /opt/mecab/lib/mecab/dic/neologd -Owakati")       # NEologd
-m = mecab.Tagger('-Owakati')        # ipadic
-
-# csvを読み込む
-df = pd.read_csv('../train_data/trans/text-only.csv', encoding='utf-8', header=0)
-
 # 分かち書きを行う発話を選定し、必要な処理を行う
 for row in df.values:
     sentence = str(row)
