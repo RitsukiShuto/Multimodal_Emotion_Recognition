7ba9344d64930a57d381ac0fccd99e26b0f0d3de
RitsukiShuto <rtk410s@gmail.com>
2022-07-16 20:15:41 +0900

    変更

--- data_formating/Split_Wav_ver2.py
+++ data_formating/Split_Wav_ver2.py
@@ -12,10 +12,11 @@ csv_dir = "../data/supervised_list.csv"
 labeled_dir = "../data/wav/labeled/"
 un_labeled_dir = "../data/wav/un_labeled/"
 
+# wavの分割を行う関数
 def split_wav(row, path):
     wav_data = AudioSegment.from_file(wav_dir + str(row[0]) + ".wav")
 
-    start = float(row[2]) * 1000
+    start = float(row[2]) * 1000        # ミリ秒に変換
     end = float(row[3]) * 1000
 
     split = wav_data[start:end]     # wavファイルを分割
@@ -26,15 +27,15 @@ def main():
     csv_file = pd.read_csv(csv_dir, encoding='UTF-8', header=0)
 
     for row in csv_file.values:
-        print(row[6])
+        print(row[6])       # DEBUG
 
-        if pd.isnull(row[9]):
-            # split_wav(row, un_labeled_dir)
+        # 感情ラベルの有無を判断
+        if pd.isnull(row[9]):       # row[9]は多数決ラベル
+            split_wav(row, un_labeled_dir)
             print("UN LABELED")
         else:
             split_wav(row, labeled_dir)
             print("LABELED")
-            
             
 
 if __name__ == "__main__":
