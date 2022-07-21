0d56235d6bc123293133280e81a344b2da2346a4
ritsukishuto <rtk410s@gmail.com>
2022-07-21 16:16:19 +0900

    微修正

--- vectorization/pow_spectrum.py
+++ vectorization/pow_spectrum.py
@@ -1,5 +1,6 @@
 # Created by RitsukiShuto on 2022/07/19.
-# Power Spectrum.py
+# pow_spectrrum.py
+# wavファイルを読み込みFFTを行い、パワースペクトルを計算する。
 #
 from json import load
 import numpy as np
@@ -9,14 +10,8 @@ import pandas as pd
 import glob
 import os
 
-full_labeled_dir = "../vector/FFT/full_labeled_pow.csv"
-half_labeled_dir = "../vector/FFT/half_labeled_pow.csv"
-un_labeled_dir = "../vector/FFT/un_labeled_pow.csv"
-
-full_labeled_pow = []
-half_labeled_pow = []
-un_labeled_pow = []
-
+# wavファイルを読み込む
+# pow_spectrum()から呼び出される
 def load_wav(filename):
     # open wave file
     wf = wave.open(filename,'r')
@@ -25,22 +20,24 @@ def load_wav(filename):
     # load wave data
     chunk_size = wf.getnframes()
     amp  = (2**8) ** wf.getsampwidth() / 2
-    data = wf.readframes(chunk_size)   # バイナリ読み込み
-    data = np.frombuffer(data,'int16') # intに変換
-    data = data / amp                  # 振幅正規化
+    data = wf.readframes(chunk_size)        # バイナリ読み込み
+    data = np.frombuffer(data,'int16')      # intに変換
+    data = data / amp                       # 振幅正規化
     data = data[::channels]
    
     return data
 
+# FFTを行いパワースペクトルを計算する
+# main()から呼び出される
 def pow_spectrum(wav_file, size):
-    st = 1000   # サンプリングする開始位置
-    fs = 44100 #サンプリングレート
-    d = 1.0 / fs #サンプリングレートの逆数
+    st = 1000               # サンプリングする開始位置
+    fs = 44100              #サンプリングレート
+    d = 1.0 / fs            #サンプリングレートの逆数
 
-    hammingWindow = np.hamming(size)    # ハミング窓
+    hammingWindow = np.hamming(size)                    # ハミング窓
     freqList = np.fft.fftfreq(size, d)
     wave = load_wav(wav_file)
-    windowedData = hammingWindow * wave[st:st+size]  # 切り出した波形データ（窓関数あり）
+    windowedData = hammingWindow * wave[st:st+size]     # 切り出した波形データ（窓関数あり）
     data = np.fft.fft(windowedData)
     data = abs(data) ** 2
 
@@ -48,30 +45,44 @@ def pow_spectrum(wav_file, size):
     
 
 def main():
+    # 出力先
+    full_labeled_dir = "../vector/pow_spectrum/full_labeled_pow.csv"
+    half_labeled_dir = "../vector/pow_spectrum/half_labeled_pow.csv"
+    un_labeled_dir = "../vector/pow_spectrum/un_labeled_pow.csv"
+
+    # パワースペクトル格納用変数
+    full_labeled_pow = []
+    half_labeled_pow = []
+    un_labeled_pow = []
+
+    # wavファイルのあるディレクトリ一覧を取得
     dir_list = glob.glob("../data/wav/*")
 
-    for dir in dir_list:
-        wav_list = glob.glob(dir + "/*.wav")
+    for dir in dir_list:                        # wavファイルのあるディレクトリを1つずつ走査
+        wav_list = glob.glob(dir + "/*.wav")    # wavファイルの一覧を取得
 
-        for wav in wav_list:
-            if dir == "../data/wav/full_labeled":
+        for wav in wav_list:                    # wavファイルを1つずつ走査
+            # ラベルの付与状態ごとに出力先を分ける
+            if dir == "../data/wav/full_labeled":       # 'emotion'ラベル付き
                 print("[FULL LABELED]" ,wav)
                 pow = pow_spectrum(wav, 128)
                 full_labeled_pow.append(pow)
-            
-            elif dir == "../data/wav/half_labeled":
+
+            elif dir == "../data/wav/half_labeled":     # 'ans_n'ラベル付き
                 print("[HALF LABELED]", wav)
                 pow = pow_spectrum(wav, 128)
                 half_labeled_pow.append(pow)
-
-            else:
+            
+            else:                                       # ラベルなし
+                # <<--------------------------------------------------------------- TODO: 声喩のみの発話をスキップする記述を追加する。
                 print("[UN LABELED]", wav)
                 pow = pow_spectrum(wav, 128)
                 un_labeled_pow.append(pow)
 
-    np.savetxt("../vector/pow_spectrum/full_labeled_pow.csv", full_labeled_pow, fmt='%8f', delimiter=',')
-    np.savetxt("../vector/pow_spectrum/half_labeled_pow.csv", half_labeled_pow, fmt='%8f', delimiter=',')
-    np.savetxt("../vector/pow_spectrum/un_labeled_pow.csv", un_labeled_pow, fmt='%8f', delimiter=',')
+    # CSVで保存
+    np.savetxt(full_labeled_dir, full_labeled_pow, fmt='%8f', delimiter=',')
+    np.savetxt(half_labeled_dir, half_labeled_pow, fmt='%8f', delimiter=',')
+    np.savetxt(un_labeled_dir, un_labeled_pow, fmt='%8f', delimiter=',')
 
 if __name__ == '__main__':
     main()
