111fe4d7d8fbb0e307e96d6f219dbb0dc9b3823d
ritsukishuto <rtk410s@gmail.com>
2022-08-19 14:35:20 +0900

    カラム名を追加する部分を削除

--- data_formating/Conversion_txt2csv.py
+++ data_formating/Conversion_txt2csv.py
@@ -10,8 +10,4 @@ save_dir = "../data/eval/"
 
 read_text_file = pd.read_csv (data_dir + file_name + extention)     # txtファイルを読み込む
 
-read_text_file.columns = ['fid', 'no', 'emotion','EF01', 'EF02', 'EF03', 'EF04', 'EF06',
-                          'EM01', 'EM02', 'EM03', 'EM04', 'EM05', 'EM06', 'EM07', 'EM08',
-                          'EM09', 'EM11', 'EM12', 'EM14', 'EM15']   # ヘッダを追加
-
 read_text_file.to_csv (save_dir + file_name + ".csv", index=False)               # 保存
