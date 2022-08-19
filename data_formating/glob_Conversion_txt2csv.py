1ed4bc0c59b7489c58632c2f185ca13257c080f5
ritsukishuto <rtk410s@gmail.com>
2022-08-19 14:35:37 +0900

    カラム名を追加する部分を削除

--- data_formating/glob_Conversion_txt2csv.py
+++ data_formating/glob_Conversion_txt2csv.py
@@ -6,18 +6,15 @@ import pandas as pd
 import glob
 import os
 
-save_dir = "../data/trans/"
-csv_list = glob.glob("../OGVC/OGVC_Vol1/Natural/trans/*.txt")
+save_dir = "../data/trans/OGVC_vol2/"
+csv_list = glob.glob("../OGVC/OGVC_Vol2/Acted/text/dlg*.txt")
 
 for csv_file in csv_list:
     print(csv_file)
     df = pd.read_csv(csv_file, header=None)
 
+    df.columns = ['no', 'person', 'emotion', 'text']   # ヘッダを追加
+    df = df.reindex(columns=['no', 'person', 'emotion', 'text'])
+
     file_name, ext = os.path.splitext( os.path.basename(csv_file))
-    #print("save to", file_name)        # DEBUG
-
-    df.columns = ['no', 'start', 'end', 'person', 'text']   # ヘッダを追加
-    df['fid'] = file_name
-    df = df.reindex(columns=['fid', 'no', 'start', 'end', 'person', 'text'])
-
     df.to_csv (save_dir + file_name + ".csv", index=False)
---
+++ data_formating/split_wav_OGVC_2.py
@@ -0,0 +1,60 @@
+# Created by RitsukiShuto on 2022/06/28.
+# 発話ごとにwavファイルを分割する。その際、{笑}のみの音声はスキップする。
+#
+from cmath import nan
+from pydub import AudioSegment
+
+import csv
+import pandas as pd
+
+# data dir
+wav_dir = "../OGVC/OGVC_Vol1/Natural/wav/"
+csv_dir = "../data/supervised_list.csv"
+
+# save dir
+full_labeled_dir = "../data/wav/full_labeled/"
+half_labeled_dir = "../data/wav/half_labeled/"
+un_labeled_dir = "../data/wav/un_labeled/"
+
+# wavファイルの分割
+def split_wav(row, path):
+    wav_data = AudioSegment.from_file(wav_dir + str(row[0]) + ".wav")
+
+    start = float(row[2]) * 1000        # ミリ秒に変換
+    end = float(row[3]) * 1000
+
+    split = wav_data[start:end]     # wavファイルを分割
+    split.export(path + str(row[0]) + "_" + str(row[1]) + ".wav", format="wav")     # 保存
+
+
+def main():
+    cnt_unlabeled = 0        # init var
+    cnt_half_labeled = 0
+    cnt_full_labeled = 0
+
+    csv_file = pd.read_csv(csv_dir, encoding='UTF-8', header=0)
+
+    for row in csv_file.values:     # メタデータを1行ずつ取得
+        print(row[6])       # DEBUG
+
+        if pd.isnull(row[6]):      # ラベルなしはスキップ
+            if row[5] != "{*}":    # '声喩'のみの発話はスキップ
+                print("[run split_wav()] UN LABELED\n")
+                split_wav(row, un_labeled_dir)
+                cnt_unlabeled += 1
+            else: 
+                print("[skip]")
+
+        elif pd.isnull(row[9]):    # 'ans_n'のみラベルあり
+            print("[run split_wav()] HALF LABELED\n")
+            split_wav(row, half_labeled_dir)
+            cnt_half_labeled += 1
+
+        else:                      # 'emotion'ラベルあり
+            print("[run split_wav()] FULL LABELED\n")
+            split_wav(row, full_labeled_dir)
+            cnt_full_labeled += 1
+            
+
+if __name__ == "__main__":
+    main()
