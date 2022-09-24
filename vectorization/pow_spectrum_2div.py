303a016f4cc136fdc6aeb4d0ec9f61eb9191c02c
ritsukishuto <rtk410s@gmail.com>
2022-09-24 14:19:57 +0900

    インデックスを変更したファイル名に変更

--- vectorization/pow_spectrum_2div.py
+++ vectorization/pow_spectrum_2div.py
@@ -73,12 +73,14 @@ def main():
                 print("[FULL LABELED]" ,wav)
                 pow = pow_spectrum(wav, 64)
                 labeled_pow.append(pow)
+                labeled_pow.insert(0, wav)
                 cnt_full_labeled += 1
 
             else:                                       # ラベルなし
                 print("[UN LABELED]", wav)
                 pow = pow_spectrum(wav, 64)
                 un_labeled_pow.append(pow)
+                un_labeled_pow.insert(0, wav)
                 cnt_un_labeled += 1
 
     # CSVで保存
@@ -88,8 +90,8 @@ def main():
     df1 = pd.DataFrame(labeled_pow)
     df2 = pd.DataFrame(un_labeled_pow)
 
-    df1.to_csv(labeled_dir, index=True, header=1)
-    df2.to_csv(un_labeled_dir, index=True, header=1)
+    df1.to_csv(labeled_dir, index=False, header=1)
+    df2.to_csv(un_labeled_dir, index=False, header=1)
 
     print("FULL LABELED:", cnt_full_labeled)
     print("UN LABELED:", cnt_un_labeled)
