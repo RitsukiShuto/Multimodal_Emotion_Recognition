5401485f6b15833e5b5c57a6d97a493eaf018ea7
RitsukiShuto <rtk410s@gmail.com>
2022-11-19 13:59:37 +0900

    ファイルを移動

--- test/mecab_test.py
+++
@@ -1,9 +0,0 @@
-import MeCab
-
-m = MeCab.Tagger("-d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd/ -Owakati")        # NEologd
-#m = MeCab.Tagger('-Owakati')        # ipadic
-
-text = "ヤバイTシャツ屋さんと岡崎体育のライブに行く"
-wakati = m.parse(text)
-
-print(wakati)
