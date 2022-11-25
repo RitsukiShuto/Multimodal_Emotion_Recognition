dcdf49167d1e4b6b7c3146d554696e564725ac91
RitsukiShuto <rtk410s@gmail.com>
2022-11-25 09:55:47 +0900

    混同行列の追加部分を実装

--- modules/supervised_learning.py
+++ modules/supervised_learning.py
@@ -2,6 +2,8 @@
 # supervised_learning.py
 # 教師あり学習を行う
 #
+import numpy as np
+
 from modules.model import model_fit
 from modules.evaluate_model import calc_score
 
@@ -13,6 +15,15 @@ def supervised_learning(X_train, Y_train, Z_train, X_test, Y_test, Z_test):
     for i in range(1):
         print(f"{i}/10")
 
+        # 学習
         multimodal_model, X_single_model, Y_single_model, model_MM, model_X, model_Y = model_fit(X_train, Y_train, Z_train, epochs)
-        conf_mat = calc_score(multimodal_model, X_single_model, Y_single_model, X_test, Y_test, Z_test)
 
+        # 精度を計算
+        MM_conf_mat = calc_score(multimodal_model, X_single_model, Y_single_model, X_test, Y_test, Z_test)
+
+        # 混同行列を計算
+        conf_mats = np.reshape(MM_conf_mat, (1, 5, 5))
+        conf_mats = np.append(conf_mats, MM_conf_mat, axis=0)
+
+        avg_conf_mat = np.average(conf_mats, axis=0)
+        print(avg_conf_mat)
