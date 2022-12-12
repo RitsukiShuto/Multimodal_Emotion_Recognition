20d049c84d1f6b7c6bd60df7b1b8e571ea713227
RitsukiShuto <rtk410s@gmail.com>
2022-12-13 02:51:55 +0900

    混同行列を学習毎に保存するように変更

--- modules/utils.py
+++ modules/utils.py
@@ -36,21 +36,34 @@ def calc_score(model_MM, model_X, model_Y, X_test, Y_test, Z_test):
     print(f"X 単一モデル\ntest loss:{X_score[0]}\ntest accuracy:{X_score[1]}\n\n")
     print(f"Y 単一モデル\ntest loss:{Y_score[0]}\ntest accuracy:{Y_score[1]}\n\n")
 
-    return MM_conf_mat
+    print("マルチモーダル混同行列")
+    print(MM_conf_mat)
 
-def calc_conf_mat(conf_mats, experiment_times, save_dir):
-    prob_conf_mats = np.zeros((experiment_times, 5, 5))
+    return MM_conf_mat, MM_score[1]
 
+def calc_conf_mat(conf_mats, experiment_times):
     labels = ['ANG', 'JOY', 'NEU', 'SAD', 'SUR']
 
     # 確率を計算
-    for i in range(experiment_times):
-        for j in range(5):
-            for k in range(5):
-                prob_conf_mats[i][j][k] = conf_mats[i][j][k] / sum(conf_mats[i, j, :])
+    if experiment_times != None:
+        prob_conf_mats = np.zeros((experiment_times, 5, 5))
 
-    prob_avg_conf_mat = np.average(prob_conf_mats, axis=0)
-    num_avg_conf_mat = np.average(conf_mats, axis=0)
+        for i in range(experiment_times):
+            for j in range(5):
+                for k in range(5):
+                    prob_conf_mats[i][j][k] = conf_mats[i][j][k] / sum(conf_mats[i, j, :])
+
+        prob_avg_conf_mat = np.average(prob_conf_mats, axis=0)
+        num_avg_conf_mat = np.average(conf_mats, axis=0)
+    
+    else:
+        prob_conf_mats = np.zeros((5, 5))
+        for i in range(5):
+            for j in range(5):
+                prob_conf_mats[i][j] = conf_mats[i][j] / sum(conf_mats[i, :])
+
+        prob_avg_conf_mat = prob_conf_mats
+        num_avg_conf_mat = conf_mats
 
     # 精度と平均を追加
     accuracy = np.trace(num_avg_conf_mat) / np.sum(num_avg_conf_mat)
@@ -63,10 +76,9 @@ def calc_conf_mat(conf_mats, experiment_times, save_dir):
     df1.loc['average', 'score'] = average
     df1.loc['accuracy', 'score'] = accuracy
 
-    df1.to_csv(save_dir+'/prob_conf_mat.csv')
-    df2.to_csv(save_dir+'/num_conf_mat.csv')
+    return df1, df2
 
-def save_fig(save_dir, multimodal_model, history_MM, num, cnt):
+def save_fig(save_dir, multimodal_model, history_MM, accuracy_trend, df1, df2, num, cnt):
     if num == 1:        # ループ1回目のときだけモデルの構成を保存
         plot_model(multimodal_model, to_file=save_dir + '/model_shape_MM.png', show_shapes=True)
 
@@ -79,8 +91,8 @@ def save_fig(save_dir, multimodal_model, history_MM, num, cnt):
         os.mkdir(save_dir + "/" + str(num))
 
     # 学習ログ(.csv)
-    df1 = pd.DataFrame(history_MM.history)      # DataFrame化
-    df1.to_csv(save_dir + "/" + str(num) + '/train_log_MM' + str(f_num) + '.csv')     # csvで保存
+    df = pd.DataFrame(history_MM.history)      # DataFrame化
+    df.to_csv(save_dir + "/" + str(num) + '/train_log_MM' + str(f_num) + '.csv')     # csvで保存
 
     # グラフ(.png)
     fig1 = plt.figure()
@@ -96,3 +108,14 @@ def save_fig(save_dir, multimodal_model, history_MM, num, cnt):
     ax2.legend(['Train'], loc='upper left')
 
     plt.savefig(save_dir + "/" + str(num) + "/MM_fig" + str(f_num) + ".png")
+
+    # 混同行列を保存
+    df1.to_csv(save_dir + "/" + str(num) + '/prob_conf_mat' + str(f_num) + '.csv')
+    df2.to_csv(save_dir + "/" + str(num) + '/num_conf_mat' + str(f_num) + '.csv')
+
+    if accuracy_trend != None:
+        fig3 = plt.figure()
+        ax3 = fig3.add_subplot(111)
+        ax3.plot(accuracy_trend)
+
+        plt.savefig(save_dir + "/" + str(num) + "/accuracy_trend" + str(f_num) + ".png")
